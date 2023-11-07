5e370366a8a1288b6cf9e5ed48d404cd2c876370
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-11-07 18:36:13 -0500

    found catan react-hex grid that works, still receiving errors even with audit force fix. Have to figure out the issue

--- battle/src/App.js
+++ battle/src/App.js
@@ -6,10 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <HexagonalBoard size={5} />
+      <HexagonalBoard size={1} />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+// import { HexGrid, Layout, Hexagon, GridGenerator, Hex } from 'react-hexgrid';
+// export default function App() {
+//   const hexagons = GridGenerator.hexagon(3);
+
+//   return (
+//     <div className="App">c
+//       <h1>Catan</h1>
+//       <HexGrid width={450} height={450} viewBox="-43 -50 100 100">
+//         <Layout size={{ x: 7, y: 7 }} spacing={1.02} flat={false}>
+//           {hexagons.map((hex: Hex, i: number) => (
+//             <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
+//           ))}
+//         </Layout>
+//       </HexGrid>
+//     </div>
+//   );
+// }
