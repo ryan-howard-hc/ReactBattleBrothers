893202d2edb3e0ca7e0f445c766a40528f30fbf5
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-11-27 21:52:59 -0500

    restarting without react-hexgrid library

--- battle/src/Board/HexBoard.js
+++ battle/src/Board/HexBoard.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import { HexGrid, Layout, Hexagon, Text, GridGenerator, Hex, HexUtils } from 'react-hexgrid';
-import configs from './configurations.json';
+import Hexagon from './Hexagon'; 
 import './board.css';
-import PlayerToken from './Player/DefaultPlayer';
-import sprite from '../Board/Player/defaultsprite.png';
 
 class HexagonalBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hexagons: [],
-      config: null,
       gridSize: { width: 100, height: 100 },
-      playerTokenPosition: { q: 0, r: 0, s: 0 }, // Store player token position
     };
   }
 
   componentDidMount() {
     this.updateGridSize();
     window.addEventListener('resize', this.updateGridSize);
-
-    // Fetch the default configuration when the component mounts
-    const defaultConfigName = 'rectangle';
-    this.loadConfiguration(defaultConfigName);
   }
 
   componentWillUnmount() {
@@ -39,62 +29,31 @@ class HexagonalBoard extends Component {
     });
   };
 
-  loadConfiguration = (configName) => {
-    const config = configs[configName];
-    if (config) {
-      const generator = GridGenerator.getGenerator(config.map);
-      const hexagons = generator.apply(this, config.mapProps);
-      this.setState({ hexagons, config });
-    }
-  };
+  renderRow = (cols, row) => {
+    const hexSize = 50; 
 
-  // Update the position of the single player token
-  updatePlayerTokenPosition = (newPosition) => {
-    this.setState({ playerTokenPosition: newPosition });
+    const hexagons = [];
+    for (let q = 0; q < cols; q++) {
+      hexagons.push(<Hexagon key={`${q}_${row}`} q={q} r={row} hexSize={hexSize} />);
+    }
+    return hexagons;
   };
 
   render() {
-    const { hexagons, config, gridSize, playerTokenPosition } = this.state;
-
-    if (!config) {
-      return null;
-    }
-
-    const layout = config.layout;
-    const size = layout ? { x: layout.width, y: layout.height } : { x: 0, y: 0 };
+    const { gridSize } = this.state;
+    const rows = 10; // Number of rows
+    const cols = 1; // Number of columns (in this case, rendering a vertical row)
 
     return (
       <div className="App">
-        <HexGrid width={gridSize.width} height={gridSize.height}>
-        {layout && (
-          <Layout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
-  {hexagons.map((hex, i) => (
-    <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s} onClick={() => this.updatePlayerTokenPosition({ q: hex.q, r: hex.r, s: hex.s })}>
-        <Text>{`${hex.q},${hex.r},${hex.s}`}</Text>
-        {/* Render the player token at coordinates 0,0,0 */}
-        {hex.q === 0 && hex.r === 0 && hex.s === 0 && (
-          <PlayerToken
-            q={hex.q}
-            r={hex.r}
-            s={hex.s}
-            spriteSrc={sprite}
-            updatePlayerTokenPosition={this.updatePlayerTokenPosition}
-          />
-        )}
-      </Hexagon>
-    ))}
-  </Layout>
-)}
-
-          {/* Render the single player token outside the hexagon loop */}
-          <PlayerToken
-            q={playerTokenPosition.q}
-            r={playerTokenPosition.r}
-            s={playerTokenPosition.s}
-            spriteSrc={sprite}
-            updatePlayerTokenPosition={this.updatePlayerTokenPosition}
-          />
-        </HexGrid>
+        <div className="hexGridContainer" style={{ width: gridSize.width, height: gridSize.height, position: 'relative' }}>
+          {/* Render vertical row of hexagons */}
+          {[...Array(rows)].map((_, index) => (
+            <div key={index} style={{ display: 'flex' }}>
+              {this.renderRow(cols, index)}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ battle/src/Board/Hexagon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './hexagon.css'; 
+
+const Hexagon = ({ q, r, hexSize }) => {
+  const calculateX = () => {
+    return (3 / 2) * hexSize * q;
+  };
+
+  const calculateY = () => {
+    return (hexSize * Math.sqrt(3)) / 2 * (2 * r + q % 2);
+  };
+
+  const x = calculateX();
+  const y = calculateY();
+
+  return (
+    <div
+      className="hexagon"
+      style={{
+        width: `${hexSize}px`,
+        height: `${hexSize}px`,
+        top: `${y}px`,
+        left: `${x}px`,
+        position: 'absolute',
+      }}
+    >
+      <div className="hexagon-content">
+        <span>{`${q},${r},${-q - r}`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Hexagon;
+
